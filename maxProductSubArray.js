74db6e88d4bb7aceb4bb227855a5c5c14bbbfd72
in hyub jeon <inhyub@inhyubs-MacBook-Air.local>
2019-03-03 13:12:33 -0800

    linear time!

--- maxProductSubArray.js
+++ maxProductSubArray.js
@@ -21,3 +21,26 @@ var maxProduct = function(nums) {
 };
 
 // O(n^2) not sure how to improve time complexity
+
+var maxProduct = function(nums) {
+    //loop through array
+    //calculate min as either num, min * num, or max * num
+    //calculate max as either num, min * num, or max * num
+    // return max
+    
+    let min = nums[0];
+    let max = nums[0];
+    let current;
+    let temp;
+    let result = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        current = nums[i];
+        temp = Math.min(current, min * current, max * current);
+        max = Math.max(current, min * current, max * current);
+        min = temp;
+        if (result < Math.max(min, max)) result = Math.max(min, max);
+    }
+    return result;
+};
+
+// linear time!
